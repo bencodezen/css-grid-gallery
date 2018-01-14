431e03ff21e2024e2ed66eb86fa689d6290fa039
Benjamin Hong <bhong@C02PF1P7G3QP.local>
2018-01-14 13:54:51 -0500

    refactor: Remove standard template component

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from '@/pages/HomePage'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
-});
+	routes: [
+		{
+			path:
+				'/',
+			name:
+				'HomePage',
+			component: HomePage
+		}
+	]
+})
