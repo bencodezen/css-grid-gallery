efb832cfb823b419130b13244d5d1693a30c183b
Benjamin Hong <bhong@C02PF1P7G3QP.local>
2018-01-14 16:10:16 -0500

    feature: Add basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-    <router-view/>
-  </div>
+		<nav>
+			<ul class="nav__list">
+				<li v-for="(item, index) in items"
+					class="nav__item"
+					:key="`nav-item-${index}`"
+				>
+					<router-link :to="item.path">{{ item.name }}</router-link>
+				</li>
+			</ul>
+		</nav>
+		<router-view/>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'App'
+	name: 'App',
+	data() {
+		return {
+			items: []
+		}
+	},
+	created() {
+		this.$router.options.routes.forEach(route => {
+			this.items.push({
+				name: route.name,
+				path: route.path
+			})
+		})
+	}
 }
 </script>
 
@@ -17,6 +40,18 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+}
+
+.nav__list {
+	display: flex;
+	list-style: none;
+}
+
+.nav__item {
+	margin-left: 1rem;
+}
+
+.nav__item:first-child {
+	margin-left: 0;
 }
 </style>
