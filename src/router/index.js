c561759804536d9535a3d477a2b435007fd88f52
Benjamin Hong <bhong@C02PF1P7G3QP.local>
2018-01-14 15:05:37 -0500

    feature: Add artwork to gallery

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/pages/HomePage'
+import CompositionC from '@/components/artwork/CompositionC'
+import CompositionWithRedYellowBlueAndBlack from '@/components/artwork/CompositionWithRedYellowBlueAndBlack'
 
 Vue.use(Router)
 
@@ -12,6 +14,20 @@ export default new Router({
 			name:
 				'HomePage',
 			component: HomePage
+		},
+		{
+			path:
+				'/art/composition-c',
+			name:
+				'CompositionC',
+			component: CompositionC
+		},
+		{
+			path:
+				'/art/composition-with-red-yellow-blue-and-black',
+			name:
+				'CompositionWithRedYellowBlueAndBlack',
+			component: CompositionWithRedYellowBlueAndBlack
 		}
 	]
 })
